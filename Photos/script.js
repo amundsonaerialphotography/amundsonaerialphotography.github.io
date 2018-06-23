e1d4a247b11c16e52e92f24cf4564ede92f4dc79
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-06-23 17:06:30 -0600

    Update script.js

--- Photos/script.js
+++ Photos/script.js
@@ -1,3 +1,4 @@
-function goBack(){
-  window.location.replace("http://amundsonaerialphotography.com/");
+function facebook() {
+  open('https://www.facebook.com/Amundson-Aerial-Photography-AAP-881108668736951/');
 }
+
