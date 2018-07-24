ec1b5c1969e428fbf78bc311f40760ae51d21576
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-24 17:22:32 -0600

    Update script.js

--- randomNumberGenerator/script.js
+++ randomNumberGenerator/script.js
@@ -6,7 +6,7 @@ function generate() {
                 alert('just telling you, if the max is "1", the random number will always be "1"');
                 alert("Maybe Moore's law doesn't apply to your brain");
                 alert("if you push 'ok' one more time, you will get you random number-- witch WILL be: 1");
-
+                document.getElementById("space").innerHTML = ('1');
 
 
 }else{
