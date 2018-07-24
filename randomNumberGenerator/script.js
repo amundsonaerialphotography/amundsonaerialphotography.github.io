af518bf2bc6c33bea7c414ec0d98a84ff7ab4b72
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-24 17:20:16 -0600

    Update script.js

--- randomNumberGenerator/script.js
+++ randomNumberGenerator/script.js
@@ -1,5 +1,14 @@
 function generate() {
         var max = document.getElementById("myNumber").value;
         console.log(max);
+   
+        if(myNumber = 1) {
+                alert('just telling you, if the max is "1", the random number will always be "1"');
+                alert("Maybe Moore's law doesn't apply to your brain");
+                alert("if you push 'ok' one more time, you will get you random number-- witch WILL be: 1");
+
+
+
+}else{
         document.getElementById("space").innerHTML = Math.floor(Math.random() * max) + 1;
-    }
+    }}
