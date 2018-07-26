45ab6e7f966bd724b3ae90eaa84814c700c2cbc4
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-26 08:32:36 -0600

    Update script.js

--- quiz/script.js
+++ quiz/script.js
@@ -1,1 +1,16 @@
 
+
+function go() {
+
+
+if (document.getElementById("no").checked) {
+  open('http://amundsonaerialphotography.com/');
+}else{
+  if (document.getElementById("yes").checked) {
+
+  window.location.replace('http://amundsonaerialphotography.com/quiz/finish/');
+}}
+
+
+}
+
