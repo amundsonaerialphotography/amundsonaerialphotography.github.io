0eff4d4466cf3c02e30f36e6dd16390443254223
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-26 09:09:17 -0600

    Update script.js

--- quiz/finish/script.js
+++ quiz/finish/script.js
@@ -1,1 +1,14 @@
 var date = document.getElementById('date').value
+let date_old;
+
+
+  if(date < 2010){
+    date_old = true;
+}else{
+  date_old = false;
+}
+
+
+function go() {
+  alert(date_old);
+}
