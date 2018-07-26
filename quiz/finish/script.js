c11487421262d54d12088947f7f325d96880a335
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-26 10:01:18 -0600

    Update script.js

--- quiz/finish/script.js
+++ quiz/finish/script.js
@@ -1,7 +1,6 @@
 
 
 function go() {
-  alert(`${dateOfPic} is way to old for a aerial photo`);
   
   var dateOfPic = document.getElementById("picDate").value;
 console.log(dateOfPic);
@@ -10,6 +9,7 @@ var date_old;
 
   if(document.getElementById("picDate").value < 2010){
     date_old = true;
+    alert(`${dateOfPic} is way to old for a aerial photo`);
 }else{
   date_old = false;
 }
