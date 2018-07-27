d02c13c6e927bfa495a6b1e41aa44ece1e39b755
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-27 12:26:44 -0600

    Update script.js

--- quiz/finish/script.js
+++ quiz/finish/script.js
@@ -51,7 +51,7 @@ if (document.getElementById("Plane").checked) {
   
   
     
-    alert(`${date_old},${AV},${colorValue},${change}.`);
+    alert(`${date_old},${AV},${colorValue},${change},--------${nedPhoto}.`);
     
   if(needPhoto == true){
     open('http://amundsonaerialphotography.com/quiz/false/');
