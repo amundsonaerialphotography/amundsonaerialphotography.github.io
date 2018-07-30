f039b8ae1eaa4552036f6e7e8f1c65aacea3e7cb
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-30 16:18:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ function facebook() {
   open('https://www.facebook.com/Amundson-Aerial-Photography-AAP-881108668736951/');
 }
 
-
+var alert = true;
 
 // Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+if(alert = true){
+modal.style.display = "block";
+alert = false;
 }
 
 // When the user clicks on <span> (x), close the modal
