4af38ceb7f6c78f5aee8652ed3b4f5cc9ec79082
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-30 16:48:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ function facebook() {
   open('https://www.facebook.com/Amundson-Aerial-Photography-AAP-881108668736951/');
 }
 
-var box = true;
-alert(`box = ${box}`);
+function modalBox() {
+  modal.style.display = "block";
+}
 
 // Get the modal
 var modal = document.getElementById('myModal');
@@ -11,11 +12,6 @@ var modal = document.getElementById('myModal');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-if(box == true){
-  modal.style.display = "block";
-  box = false;
-}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
