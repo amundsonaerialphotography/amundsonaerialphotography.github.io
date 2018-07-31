57e0b0eb9069e3ba35811508d928885d26f36eaf
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-30 19:11:08 -0600

    Create script.js

---
+++ quiz/true/script.js
@@ -0,0 +1,6 @@
+var date_old;
+var AV;
+var colorValue;
+var change;
+
+alert(`Your photo was ${date_old}ly too old, The aerial veicle used was a ${AV}, the your photo was in ${colorValue}, and the amount of change to your farm since the last photo was ${change}.`);
