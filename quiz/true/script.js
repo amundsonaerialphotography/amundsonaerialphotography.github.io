7b25413ca1ade9d31e3d7477b027cc39e109ff0c
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2018-07-31 08:44:00 -0600

    Update script.js

--- quiz/true/script.js
+++ quiz/true/script.js
@@ -4,6 +4,7 @@ var colorValue;
 var change;
 
 document.getElementById('colorButton').onclick = function () {
+  alert("clicked");
   document.getElementById('body').style.font-family = "sans-serif";
 }
 //alert(`Your photo was ${date_old}ly too old, The aerial veicle used was a ${AV}, the your photo was in ${colorValue}, and the amount of change to your farm since the last photo was ${change}.`);
